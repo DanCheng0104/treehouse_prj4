a26ba17f6566c7e3ecb87d465fcdfb0934321f0b
DAN CHENG <dcheng0104@gmail.com>
2017-08-23 16:05:15 -0700

    fix the codes so now it could either play with a computer or computuer

--- tic-tac-toe-v3/js/ui.js
+++ tic-tac-toe-v3/js/ui.js
@@ -1,5 +1,7 @@
 (function($) {
+    //this is when the user click the yes button on the start page, so it will play against computer
     $('#yes').on('click',()=>{
+        //remove all the bottoms and player p tag
          $('#yes').remove();
          $('#no').remove();
          $('p').remove();
@@ -11,16 +13,17 @@
              let person = $('#p1').val();          
              $('#start').hide();
              $('#board').show();
-             playingWithComputer(person);
+             playing(person);
          });
     });
-
+    //this is when the user click the no button on the start page, so it will play against computer
     $('#no').on('click',()=>{
+        //remove all the bottoms and player p tag
          $('#yes').remove();
          $('#no').remove();
          $('p').remove();
          let html =`<p>
-            Player1: <input type="text" id="p1" value="Mickey">
+            Player1: <input type="text" id="p1" value="Nate">
             Player2: <input type="text" id="p2" value="Mickey">
          </p><a href="#" id='startBtn' class="button">Start game</a>`
          $(html).insertAfter('#start h1');
@@ -38,13 +41,11 @@
         clearBoard();
         let p1 = $('#p1_name').html();
         let p2 = $('#p2_name').html();
-
-        (p2  === "")? playingWithComputer(p1):playing(p1,p2);
-        
-
+        (p2  === "")? playing(p1):playing(p1,p2);        
     });
-	showOneTemplate('#start');
 
+	showOneTemplate('#start');
+    //clear the board when game is over
     function clearBoard(){
         $('.boxes li').removeClass('box-filled-1');
         $('.boxes li').removeClass('box-filled-2');
@@ -53,24 +54,23 @@
         $('#player2').removeClass('active'); 
         $('.boxes li').removeAttr('clicked');
     }
-
+    //by default all the three html templates are in the html, the
     function showOneTemplate(targetTemplate){
         templates = ['#start','#board','#finish']
         templates.forEach((template)=>$(template).hide());
         $(targetTemplate).show();
     
     }
-
+    //remove all the listners on the board
     function removeListeners(){
         $('.boxes').off("click");
         $('.boxes').off("mouseover");
         $('.boxes').off("mouseout");       
     }
 
+    //check if the player wins
     function win(score){
         const wins = [7, 56, 448, 73, 146, 292, 273, 84];
-<<<<<<< HEAD
-=======
         for (let i = 0; i < wins.length; i += 1) {
             if ((wins[i] & score) === wins[i]) {
                 return true;
@@ -79,25 +79,39 @@
         return false;
     }
 
-    $('.boxes').on('mouseover',(event)=>{
-            if (!$(event.target).attr('clicked')){
-                if ($('#player1').hasClass("active")){$(event.target).css("background-image",'url(img/o.svg)');}
-                else{$(event.target).css("background-image",'url(img/x.svg)');}
-                
-            }
->>>>>>> b9ef76910ff55ef827581c26cc1fce0e6d9d98eb
+    //showing correct class based on which player wins
+    function polishFinish(class_added,text){
+        showOneTemplate('#finish');
+        const classes = ['screen-win-one','screen-win-two','screen-win-tie'];
+        classes.forEach(x => $('#finish').removeClass(x));
+        $('#finish').addClass(class_added);  
+        $('h4').remove(); 
+        $(`<h4 class='name'>${text}</h4>`).insertAfter('#finish h1');
+        removeListeners(); 
+    }
 
-    });
-    $('.boxes').on('mouseout',(event)=>{
-            if (!$(event.target).attr('clicked')){
-                $(event.target).css("background-image",'');
-            }
-    }); 
-
+    //core function in this code, when person 2 is not defined, it will play with computer
     function playing(person1,person2){
         const wins = [7, 56, 448, 73, 146, 292, 273, 84];
+        //set player 1's name on the board
         $('#p1_name').html(person1);
-        if (person2 !=='') {$('#p2_name').html(person2)};
+        let cells = $('.boxes .box');
+        for (let i=0;i<9;i++){
+            let value = Math.pow(2,i);
+            $(cells[i]).attr("value",value);
+        }  
+
+        if (person2 !=='') {
+            //set player 2's name on the board
+            $('#p2_name').html(person2);
+        }
+
+        let lists = [0,1,2,3,4,5,6,7,8];
+        //give each cell value, from which we calculate the score, based on the score, we would know if the player wins
+        lists.forEach(list=>{
+            let value = Math.pow(2,list);
+            $(cells[list]).attr("value",value);
+        })
         let score= {'X':0,'O':0};
         let moves = 0;
         let turn = 'O';     
@@ -106,151 +120,9 @@
             'X':{'background-image':'url(img/x.svg)','id':'#player2','box-style':'box-filled-2'},
             'O':{'background-image':'url(img/o.svg)','id':'#player1','box-style':'box-filled-1'}
         }       
-        let cells = $('.boxes .box');
-        for (let i=0;i<9;i++){
-            let value = Math.pow(2,i);
-            $(cells[i]).attr("value",value);
-        }  
 
-        $('.boxes').click(function(event){    
-            score[turn] = score[turn] + event.target.value;
-            moves+=1; 
-            if (win(score[turn])) {
-                showOneTemplate('#finish');
-                turn === 'O'?$('#finish').addClass('screen-win-one'):$('#finish').addClass('screen-win-two');
-                removeListeners(); 
-            } else if (moves === 9) {
-                showOneTemplate('#finish');
-                $('#finish').removeClass('screen-win-one');
-                $('#finish').removeClass('screen-win-two');    
-                $('#finish').addClass('screen-win-tie');    
-                removeListeners();                  
-                
-            } else {
-                $(event.target).css("background-image",players[turn]['background-image']);
-                $(event.target).attr("clicked",1);
-                $(event.target).addClass(players[turn]['box-style']);
-                $(players[turn]['id']).removeClass('active');
-                turn= turn==='O'?'X':'O';
-                $(players[turn]['id']).addClass('active');
-            }
-
-        });
-    }
-
-    function playingWithPerson(person1,person2){
-        //initiate the value
-        // const wins = [7, 56, 448, 73, 146, 292, 273, 84];
-        // $('#p1_name').html(person1);
-        // $('#p2_name').html(person2);
-        // let score= {'X':0,'O':0};
-        // let moves = 0;
-        // let turn = 'O';     
-        // $('#player1').addClass('active');
-        // let players = {
-        //     'X':{'background-image':'url(img/x.svg)','id':'#player2','box-style':'box-filled-2'},
-        //     'O':{'background-image':'url(img/o.svg)','id':'#player1','box-style':'box-filled-1'}
-        // }       
-        // let cells = $('.boxes .box');
-        // for (let i=0;i<9;i++){
-        //     let value = Math.pow(2,i);
-        //     $(cells[i]).attr("value",value);
-        // }
-        // playing(person1,person2);
-
-        // $('.boxes').click(function(event){    
-        //     score[turn] = score[turn] + event.target.value;
-        //     moves+=1; 
-        //     if (win(score[turn])) {
-        //         showOneTemplate('#finish');
-        //         turn === 'O'?$('#finish').addClass('screen-win-one'):$('#finish').addClass('screen-win-two');
-        //         removeListeners(); 
-        //     } else if (moves === 9) {
-        //         showOneTemplate('#finish');
-        //         $('#finish').removeClass('screen-win-one');
-        //         $('#finish').removeClass('screen-win-two');    
-        //         $('#finish').addClass('screen-win-tie');    
-        //         removeListeners();                  
-                
-        //     } else {
-        //         $(event.target).css("background-image",players[turn]['background-image']);
-        //         $(event.target).attr("clicked",1);
-        //         $(event.target).addClass(players[turn]['box-style']);
-        //         $(players[turn]['id']).removeClass('active');
-        //         turn= turn==='O'?'X':'O';
-        //         $(players[turn]['id']).addClass('active');
-        //     }
-
-        // });
-    }
-
-	function playingWithComputer(person){
-        //initiate the value
         
-        let score= {'X':0,'O':0};
-        let moves = 0;
-        let turn = 'O';     
-        $('#player1').addClass('active');
-        $('#p1_name').html(person);
-
-        let players = {
-            'X':{'background-image':'url(img/x.svg)','id':'#player2','box-style':'box-filled-2'},
-            'O':{'background-image':'url(img/o.svg)','id':'#player1','box-style':'box-filled-1'}
-        }		
- 		let cells = $('.boxes .box');
-        let lists = [0,1,2,3,4,5,6,7,8];
-        lists.forEach(list=>{
-            let value = Math.pow(2,list);
-            $(cells[list]).attr("value",value);
-        })
-
- 		$('.boxes').click(function(event){  
-            let person = $('#player1').html();  
-            let oVal;
-            let xVal;
-			score[turn] = score[turn] + event.target.value;
-			moves+=2; 
-            console.log(lists);
-			if (win(score[turn])) {
-            	showOneTemplate('#finish');
-                if (turn === 'O'){
-                     $('.name').remove();
-                     $('#finish').removeClass('screen-win-two');
-                    $('#finish').addClass('screen-win-one');
-                    $('#finish').removeClass('screen-win-tie');    
-                    $(`<h4 class='name'>${person} wins</h4>`).insertAfter('#finish h1');
-                }
-                else {
-                    $('#finish').removeClass('screen-win-one');
-                    $('#finish').addClass('screen-win-two');
-                    $('#finish').removeClass('screen-win-tie');    
-                    $('.name').remove();
-                }
-                removeListeners(); 
-            } else if (moves > 9) {
- 	            showOneTemplate('#finish');
-                $('#finish').removeClass('screen-win-one');
-                $('#finish').removeClass('screen-win-two');    
-                $('#finish').addClass('screen-win-tie');	
-                $('.name').remove();
-                removeListeners();             		
-            	
-        	} else {
-                $(event.target).css("background-image",players[turn]['background-image']);
-                $(event.target).attr("clicked",1);
-                $(event.target).addClass(players[turn]['box-style']);
-                oVal = parseInt($(event.target).attr("order"));
-                lists.splice(lists.indexOf(oVal),1);
-                $(players['O']['id']).removeClass('active');
-                $(players['X']['id']).addClass('active');
-                computerPlaying(oVal);
-
-        	}
-
-		});
-
         function computerPlaying(oVal){
-
             let yVal = lists[Math.floor(Math.random( ) *lists.length )];
             $( ".boxes li[order=" + yVal + "]" ).attr("clicked",1);
             $( ".boxes li[order=" + yVal + "]" ).css("background-image",players['X']['background-image']);
@@ -260,24 +132,70 @@
             $(players['O']['id']).addClass('active');
             score['X'] = score['X'] + parseInt($( ".boxes li[order=" + yVal + "]" ).attr('value'));
             if (win(score['X'])) {
-                showOneTemplate('#finish');
-                $('#finish').removeClass('screen-win-one');
-                $('#finish').addClass('screen-win-two');    
-                $('#finish').removeClass('screen-win-tie');    
-                $('.name').remove();
-                removeListeners(); 
+                polishFinish('screen-win-two','Computer wins!');   
             }
-        }
+        }  
 
+        $('.boxes').on('mouseover',(event)=>{
+            if (!$(event.target).attr('clicked')){
+                if ($('#player1').hasClass("active")){$(event.target).css("background-image",'url(img/o.svg)');}
+                else{$(event.target).css("background-image",'url(img/x.svg)');}            
+            }
 
+        });
+        $('.boxes').on('mouseout',(event)=>{
+            if (!$(event.target).attr('clicked')){
+                    $(event.target).css("background-image",'');
+            }
+        });      
 
-	
+        $('.boxes').click(function(event){    
+            console.log(person1);
+            console.log(person2);
+            //when two players are playing
+            if (person2 !== undefined) {
+                score[turn] = score[turn] + event.target.value;
+                moves+=1; 
+                if (win(score[turn])) {
+                    (turn === 'O')?polishFinish('screen-win-one',person1 + ' wins!'):polishFinish('screen-win-two',person2 + ' wins!');   
+                } else if (moves === 9) {
+                    polishFinish('screen-win-tie','It\'s a tie!');           
+                
+                } else {
+                    $(event.target).css("background-image",players[turn]['background-image']);
+                    $(event.target).attr("clicked",1);
+                    $(event.target).addClass(players[turn]['box-style']);
+                    $(players[turn]['id']).removeClass('active');
+                    turn= turn==='O'?'X':'O';
+                    $(players[turn]['id']).addClass('active');
+                }
+              
+            }
+            //when playing against computer
+            else{
+                let oVal;
+                score[turn] = score[turn] + event.target.value;
+                moves+=2; 
+                if (win(score[turn])) {
+                    polishFinish('screen-win-one',person1 + ' wins!')
+                } else if (moves > 9) {
+                    polishFinish('screen-win-two',person2 + ' wins!');                                    
+                } else {
+                    $(event.target).css("background-image",players[turn]['background-image']);
+                    $(event.target).attr("clicked",1);
+                    $(event.target).addClass(players[turn]['box-style']);
+                    oVal = parseInt($(event.target).attr("order"));
+                    lists.splice(lists.indexOf(oVal),1);
+                    $(players['O']['id']).removeClass('active');
+                    $(players['X']['id']).addClass('active');
+                    computerPlaying(oVal);
 
-	}
+                }
 
+            }
 
+        });
 
-
-
+    }
 
 })(jQuery);
